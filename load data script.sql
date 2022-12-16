70b26a737a8513427725cbe4abae46e2d4b764f1
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-16 21:55:21 +0200

    try map by hand

--- load data script.sql
+++ load data script.sql
@@ -4,35 +4,37 @@ SET GLOBAL local_infile=1;
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\PUBLISHER.csv" into table sample19015550.publisher
 fields terminated by ','
 lines terminated by '\n'
-ignore 1 lines;
+ignore 1 lines
+(sample19015550.publisher.Name,sample19015550.publisher.Address,sample19015550.publisher.Phone);
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\BOOK.csv" into table sample19015550.book
 fields terminated by ','
 lines terminated by '\n'
-ignore 1 lines;
+ignore 1 lines
+(sample19015550.book.BookId,sample19015550.book.Title,sample19015550.book.PublisherName);
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\BOOK_AUTHORS.csv" into table sample19015550.book_authors
-fields terminated by ',' ENCLOSED BY '"' ESCAPED BY '\\'
+fields terminated by ',' 
 lines terminated by '\n'
 ignore 1 lines;
 
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\BORROWER.csv" into table sample19015550.borrower
-fields terminated by ',' ENCLOSED BY '"' ESCAPED BY '\\'
+fields terminated by ','
 lines terminated by '\n'
 ignore 1 lines;
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\LIBRARY_BRANCH.csv" into table sample19015550.library_branch
-fields terminated by ',' ENCLOSED BY '"' ESCAPED BY '\\'
+fields terminated by ','
 lines terminated by '\n'
 ignore 1 lines;
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\BOOK_LOANS.csv" into table sample19015550.book_loans
-fields terminated by ',' ENCLOSED BY '"' ESCAPED BY '\\'
+fields terminated by ','
 lines terminated by '\n'
 ignore 1 lines;
 
 load data local infile "D:\\College\\term5\\Data Base\\Database_lab_3\\Data tables\\BOOK_COPIES.csv" into table sample19015550.book_copies
-fields terminated by ',' ENCLOSED BY '"' ESCAPED BY '\\'
+fields terminated by ',' 
 lines terminated by '\n'
 ignore 1 lines;
